7142de140d66fcfce96a60af0553dbe0b2f8ef8d
dduskxy <dan.monthon@gmail.com>
2026-09-16 22:42:02 +0700

    Fix syntax error caused by extra closing div in SlideLayout

--- src/components/slides/SlideLayout.tsx
+++ src/components/slides/SlideLayout.tsx
@@ -62,7 +62,6 @@ export function SlideLayout({
                     </p>
                   </div>
                 </div>
-                </div>
 
                 {/* The Interactive Content */}
                 <div className="flex-1 bg-white/[0.02] border border-white/10 rounded-3xl p-4 md:p-6 shadow-2xl relative overflow-y-auto min-h-0 flex flex-col">
